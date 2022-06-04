3de92634c6485518dc1f71d0d5ce522df57e2754
Nicolas Knaian <nickknaian@gmail.com>
2022-06-03 22:24:30 -0400

    Stop currently running service when deploying

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Log in and stop the currently running service
+ssh rem "systemctl stop rm_text_share"
+
 # Copy binary and service files to remarkable
 scp target/armv7-unknown-linux-gnueabihf/release/rm_text_share rem:/usr/bin
 scp rm_text_share.service rem:/etc/systemd/system
