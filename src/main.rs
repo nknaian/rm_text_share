ee1e2c4937b5ffa9d016367b7a3183c0333105ab
Nicolas Knaian <nickknaian@gmail.com>
2022-06-03 22:26:01 -0400

    Watch xochitl directory for changes, print to log

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use syslog::{Facility, Formatter3164, BasicLogger};
-use std::thread::sleep;
+use notify::{watcher, RecursiveMode, Watcher};
+use std::sync::mpsc::channel;
+use syslog::{BasicLogger, Facility, Formatter3164};
 
 fn main() {
     // Setup logging to syslog (at info level)
@@ -9,19 +10,39 @@ fn main() {
         process: "rm_text_share".into(),
         pid: 0,
     };
-    
+
     let logger = match syslog::unix(formatter) {
-        Err(e) => { println!("impossible to connect to syslog: {:?}", e); return; },
+        Err(e) => {
+            println!("impossible to connect to syslog: {:?}", e);
+            return;
+        }
         Ok(logger) => logger,
     };
     let _ = log::set_boxed_logger(Box::new(BasicLogger::new(logger)))
-            .map(|()| log::set_max_level(log::LevelFilter::Info));
+        .map(|()| log::set_max_level(log::LevelFilter::Info));
 
     log::info!("Started rm_text share");
 
-    // Main loop
+    // Create a channel to receive the events.
+    let (tx, rx) = channel();
+
+    // Create a watcher object, delivering debounced events.
+    // The notification back-end is selected based on the platform.
+    let mut watcher = watcher(tx, std::time::Duration::from_secs(10)).unwrap();
+
+    // Add a path to be watched. All files and directories at that path and
+    // below will be monitored for changes.
+    watcher
+        .watch(
+            "/home/root/.local/share/remarkable/xochitl",
+            RecursiveMode::Recursive,
+        )
+        .unwrap();
+
     loop {
-        sleep(std::time::Duration::from_secs(10));
-        log::info!("Another 10 seconds have elapsed, hurrah!");
+        match rx.recv() {
+            Ok(event) => println!("{:?}", event),
+            Err(e) => println!("watch error: {:?}", e),
+        }
     }
 }
